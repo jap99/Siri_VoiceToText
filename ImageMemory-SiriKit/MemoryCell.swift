fdda81523ec9348f60d4c584cfd4a81de9fe92db
javidp.sf@gmail.com <javidpoornasir@Javids-MacBook-Pro-2.local>
2018-01-24 11:18:10 -0800

    Permissions added

---
+++ ImageMemory-SiriKit/MemoryCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemoryCell.swift
+//  ImageMemory-SiriKit
+//
+//  Created by Javid Poornasir on 1/24/18.
+//  Copyright © 2018 Javid Poornasir. All rights reserved.
+//
+
+import UIKit
+
+class MemoryCell: UICollectionViewCell {
+    
+}
